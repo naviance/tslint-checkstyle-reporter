e75e61762b841e1efc60fefba864c0e0beb49953
Pickra <ari.picker@hobsons.com>
2015-11-20 14:24:59 -0500

    Getting xml output

--- src/xmlFormatter.ts
+++ src/xmlFormatter.ts
@@ -1,17 +1,63 @@
 /// <reference path="../node_modules/tslint/typings/typescriptServices.d.ts" />
 /// <reference path="../node_modules/tslint/lib/tslint.d.ts" />
-/// <reference path="../typings/node.d.ts" />
 
-var fs = require("fs");
+var xml = require('xml');
+
+interface XmlObjectFormat {
+    file: [
+        { _attr: { name: string } },
+        { error: [
+            {_attr: {
+                line: string;
+                column: string;
+                message: string;
+                severity: string;
+                source: string;
+            }}
+        ]}
+    ];
+}
+
+interface jsonObj {
+    name: string;
+    startPosition: { line: string, position: string };
+    ruleName: string;
+    failure: string;
+}
 
 export class Formatter extends Lint.Formatters.AbstractFormatter {
-    public format(failures: Lint.RuleFailure[]): string {
-        var failuresJSON = failures.map((failure: Lint.RuleFailure) => failure.toJson());
+    xmlObjFormat: XmlObjectFormat = {
+        file: [
+            { _attr: { name: "" } },
+            {
+                error: [
+                    {
+                        _attr: {
+                            line: "",
+                            column: "",
+                            message: "",
+                            severity: "warning",
+                            source: ""
+                        }
+                    }
+                    ]
+            }
+            ]
+    };
 
-        fs.writeFileSync("outputFile.txt", JSON.stringify(failuresJSON), "utf-8", function(err){
-            if (err) { throw err; }
+    returnWithNoStrings (value: string): string { return value.replace(/'/g, ""); }
+
+    public format(failures: Lint.RuleFailure[]): string {
+        let failuresJSON = failures.map((failure: Lint.RuleFailure) => failure.toJson());
+
+        failuresJSON.forEach((obj: jsonObj) => {
+            this.xmlObjFormat.file[0]._attr.name                 = obj.name;
+            this.xmlObjFormat.file[1].error[0]._attr.line        = obj.startPosition.line;
+            this.xmlObjFormat.file[1].error[0]._attr.column      = obj.startPosition.position;
+            this.xmlObjFormat.file[1].error[0]._attr.source      = obj.ruleName;
+            this.xmlObjFormat.file[1].error[0]._attr.message     = this.returnWithNoStrings(obj.failure);
         });
 
-        return JSON.stringify(failuresJSON);
+        return xml(this.xmlObjFormat);
     }
 }
