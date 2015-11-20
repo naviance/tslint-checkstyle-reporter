68fa291dfa76a98822a12a6997f19bfc680b71bb
Pickra <ari.picker@hobsons.com>
2015-11-20 14:24:59 -0500

    Getting xml output

--- src/xmlFormatter.ts
+++ src/xmlFormatter.ts
@@ -1,17 +1,39 @@
-/// <reference path="../node_modules/tslint/typings/typescriptServices.d.ts" />
-/// <reference path="../node_modules/tslint/lib/tslint.d.ts" />
 /// <reference path="../typings/node.d.ts" />
+// TODO: add typings back in - https://github.com/palantir/tslint#writing-custom-formatters
+var xml = require('xml');
 
-var fs = require("fs");
+export class Formatter {
+    removeSingleQuotes (value: string): string {
+        let regEx = /'/g;
+        if(value.match(regEx)) { return value.replace(regEx, ""); }
 
-export class Formatter extends Lint.Formatters.AbstractFormatter {
-    public format(failures: Lint.RuleFailure[]): string {
-        var failuresJSON = failures.map((failure: Lint.RuleFailure) => failure.toJson());
+        return value;
+    }
 
-        fs.writeFileSync("outputFile.txt", JSON.stringify(failuresJSON), "utf-8", function(err){
-            if (err) { throw err; }
+    public format(failures): string {
+        let failuresJSON = failures.map((failure) => failure.toJson());
+        let checkstyleFileObject: any = { file: [{ _attr: { name: "" }}] };
+
+        failuresJSON.forEach((obj) => {
+            checkstyleFileObject.file[0]._attr.name = obj.name;
+
+            let err = {
+                error: [
+                    {
+                        _attr: {
+                            line: obj.startPosition.line,
+                            column: obj.startPosition.position,
+                            message: this.removeSingleQuotes(obj.failure),
+                            severity: "warning",
+                            source: obj.ruleName,
+                        }
+                    }
+                ]
+            };
+
+            checkstyleFileObject.file.push(err);
         });
 
-        return JSON.stringify(failuresJSON);
+        return xml(checkstyleFileObject) as string;
     }
 }
