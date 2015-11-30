e5a8e0d24930f3e483116e22fd516bd317fbbc5d
Pickra <ari.picker@hobsons.com>
2015-11-30 13:02:16 -0500

    Ungitignored xmlFormatter.js

---
+++ src/xmlFormatter.js
@@ -0,0 +1,38 @@
+/// <reference path="../typings/node.d.ts" />
+var xml = require('xml');
+var Formatter = (function () {
+    function Formatter() {
+    }
+    Formatter.prototype.removeSingleQuotes = function (value) {
+        var regEx = /'/g;
+        if (value.match(regEx)) {
+            return value.replace(regEx, "");
+        }
+        return value;
+    };
+    Formatter.prototype.format = function (failures) {
+        var _this = this;
+        var failuresJSON = failures.map(function (failure) { return failure.toJson(); });
+        var checkstyleFileObject = { file: [{ _attr: { name: "" } }] };
+        failuresJSON.forEach(function (obj) {
+            checkstyleFileObject.file[0]._attr.name = obj.name;
+            var err = {
+                error: [
+                    {
+                        _attr: {
+                            line: obj.startPosition.line,
+                            column: obj.startPosition.position,
+                            message: _this.removeSingleQuotes(obj.failure),
+                            severity: "warning",
+                            source: obj.ruleName,
+                        }
+                    }
+                ]
+            };
+            checkstyleFileObject.file.push(err);
+        });
+        return xml(checkstyleFileObject);
+    };
+    return Formatter;
+})();
+exports.Formatter = Formatter;
